3e8959ac620002bda13f65d02261d95dc7e85bdd
Jack Fogarty <jackfogarty2105@gmail.com>
2021-06-10 16:15:04 +0800

    Removed force: true

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
 
     console.log(
